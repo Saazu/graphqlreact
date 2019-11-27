3fc01937c004dec8565007884d224fa12919727c
Alfred Delle <alfred.delle@yale.edu>
2019-11-27 20:27:58 +0000

    Added Person component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
+import Persons from './Persons'
 
 const ALL_PERSONS = gql`
 {
@@ -16,16 +17,7 @@ const ALL_PERSONS = gql`
 const App = () => {
   return (
     <Query query={ALL_PERSONS}>
-      {(result) => {
-        if (result.loading) {
-          return <div>Loading</div>
-        }
-        return (
-          <div>
-            {result.data.allPersons.map(p => p.name).join(', ')}
-          </div>
-        )
-      }}
+      {(result) => <Persons result={result} />}
     </Query>
   )
 }
